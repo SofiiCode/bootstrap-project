68f0b618b97be15e52786c952365d005c590fc6e
SofiiCode <sofi.andand06@gmail.com>
2023-01-24 09:52:23 +0200

    github cards

--- index.js
+++ index.js
@@ -1,65 +1,95 @@
-const hireable = document.querySelector("#string");
-const userName = "cruftyoldsysadmin";
-const repoName = "dev-profile";
-function displayDataUser(response) {
-  console.log(response);
-  let icon = document.querySelector("#icon");
-  icon.setAttribute("src", `${response.data.avatar_url}`);
 
-  let name = document.querySelector("#name");
-  name.innerHTML = `${response.data.name}`;
-name.setAttribute("href", `${response.data.html_url}`);
-  let subname = document.querySelector("#subname");
-  subname.innerHTML = `@${response.data.login}`;
-  subname.setAttribute("href", `${response.data.html_url}`);
 
-  let followBtn = document.querySelector("#follow");
-  followBtn.setAttribute("href", `${response.data.html_url}`);
+function displayDataUser (response){
+  let userCardElement = document.querySelector("#user-card");
+  let userCardHTML = `<div class="header d-flex flex-wrap justify-content-between">`;
+  userCardHTML =
+    userCardHTML +
+    `<img src="${response.data.avatar_url}" class="rounded-circle img-start"  width="80" height="80"alt="user-photo">
+                    <div class="user-name m-2">
+                      <h5 class="card-title "><a href="${response.data.html_url}"  class="name-link">${response.data.name}</a></h5>
+                      <p class="card-text "><a href="${response.data.html_url}"  class="sub-name-link">@${response.data.login}</a></p>
+                    </div>
+                    <a href="${response.data.html_url}"  class="btn btn-dark btn-follow" >Follow</a>
+                </div>
+                <div class="status d-flex mt-2 mb-2 p-2 text-uppercase border-light-subtle border-top border-bottom">
+                  <div class="pe-3 ps-3 border-light border-end  ">
+                    <h5 id="repos" class="card-title">${response.data.public_repos}</h5>
+                  <p class="card-text"><a href="https://github.com/${response.data.login}?tab=repositories" >repos</a></p>
+                  </div>
+                  <div class=" pe-3 ps-3 border-light border-end  ">
+                  <h5 id="gists" class="card-title">${response.data.public_gists}</h5>
+                  <p  class="card-text"><a  href="https://gist.github.com/${response.data.login}">gists</a></p>
+                  </div>
+                  <div class="ps-3  ">
+                    <h5 class="card-title">${response.data.followers}</h5>
+                  <p class="card-text"><a   href="https://github.com/${response.data.login}?tab=followers">followers</a></p>
+                  </div>
+                </div>
+                <div class="footer p-2 ">
+                  <p class="card-subtitle fw-bold"> <a id="string" href="${response.data.html_url}"></a></p>
+                </div>`;
 
-  let repos = document.querySelector("#repos");
-  repos.innerHTML = `${response.data.public_repos}`;
-  let reposLink = document.querySelector("#repos-link");
-  reposLink.setAttribute(
-    "href",
-    `https://github.com/${response.data.login}?tab=repositories`
-  );
-  let gists = document.querySelector("#gists");
-  gists.innerHTML = `${response.data.public_gists}`;
-  let gistsLink = document.querySelector("#gists-link");
-  gistsLink.setAttribute(
-    "href",
-    `https://gist.github.com/${response.data.login}`
-  );
-  let followers = document.querySelector("#followers");
-  followers.innerHTML = `${response.data.followers}`;
-  let followersLink = document.querySelector("#followers-link");
-  followersLink.setAttribute(
-    "href",
-    `https://github.com/${response.data.login}?tab=followers`
-  ); 
   
-  if (`${response.data.hireable}` == "true") {
-    hireable.innerHTML = "Available for hire";
-  } else if (`${response.data.hireable} ` == "false") {
-    hireable.innerHTML = "Not available for hire.";
-  } else {
-    hireable.innerHTML = "else";
-  }
+  userCardHTML = userCardHTML + `</div>`;
+  userCardElement.innerHTML = userCardHTML;
+
+    if (`${response.data.hireable}` == "true") {
+        let footer = document.querySelector('#string') 
+      footer.innerHTML = "Available for hire";
+    } else if (`${response.data.hireable} ` == "false") {
+        let footer = document.querySelector("#string"); 
+      footer.innerHTML = "Not available for hire";
+    } else {
+        let footer = document.querySelector("#string"); 
+      footer.innerHTML = "else";
+    }
 }
 
-function getDataUser(userName) {
-
+function getDataUser() {
+let userName = document.querySelector("#user-name").innerHTML;
   let apiUrl = `https://api.github.com/users/${userName}`;
   axios.get(apiUrl).then(displayDataUser);
 }
-getDataUser(userName);
+getDataUser();
 
 function displayDataRepo(response) {
   console.log(response);
+  let userRepoElement = document.querySelector("#repo-card");
+ 
+  let userRepoHTML = `<div class="header d-flex justify-content-between">`;
+    userRepoHTML =
+      userRepoHTML +
+      `<img src="${response.data.owner.avatar_url}" class="rounded-circle img-start"  width="80" height="80"alt="user-photo">
+                    <div class="user-name ms-1">
+                      <h5 class="card-title "><a href="${response.data.html_url}" class="repo-name-link">${response.data.name}</a></h5>
+                      <p class="card-text ">Forked by: <a href="${response.data.owner.html_url}" class="sub-name-link">@${response.data.owner.login}</a></p>
+                    </div>
+                    <a href="${response.data.html_url}" class="btn btn-dark btn-follow" >Star</a>
+                </div>
+                <div class="git-description border-bottom  d-flex mt-2 mb-2 p-2 ">
+                  <div class="pe-3 ps-3 fs-6">
+                  <p class="card-text">The unofficial GitHub Cards. Card for your GitHub profile, card for your GitHub repositories.
+                    <a  href="https://lab.lepture.com/github-cards/" >lab.lepture.com/github-cards</a>
+                  </p>
+                  </div>
+                </div>
+                <div class="footer d-flex text-uppercase">
+                  <div class="ps-2  d-flex ">
+                    <p class="fw-bold m-0 pe-1">${response.data.forks}</p>
+                    <p class="card-text">forks</p>
+                  </div>
+                  <div class="ps-2 d-flex ">
+                    <p class="fw-bold m-0 pe-1">1</p>
+                    <p class="card-text">stars</p>
+                  </div>
+                  </div>`;
+  userRepoHTML = userRepoHTML + '</div>';
+                  userRepoElement.innerHTML = userRepoHTML
 }
-function getDataRepo(repoName) {
-
+function getDataRepo() {
+let repoName= document.querySelector('#repo-name').innerHTML
   let apiUrl = `https://api.github.com/repos/cruftyoldsysadmin/${repoName}`;
   axios.get(apiUrl).then(displayDataRepo);
 }
-getDataRepo(repoName)
+getDataRepo()
